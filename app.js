5cbb0257cbd8edb88ea54e88f8b25633e6a944bb
Aravind <gmarav005@gmail.com>
2025-08-12 09:51:21 +0530

    last step

--- app.js
+++ app.js
@@ -6,96 +6,131 @@ let num = 0;
 let str = "";
 
 function getComputerChoice(str) {
-  num = (Math.floor(Math.random() * 2)) * 1 ;
+
+  num = Math.floor(Math.random() * 2) * 1;
 
   console.log(num);
 
-  if (num === 1 ) {
+  if (num === 1) {
     return "rock";
-
   } else {
     return "paper";
-
   }
-
 }
 
 const computerSelection = getComputerChoice(str);
 
-
-
-
 // // user input
 
 function getHumanChoice() {
-    return prompt("");
+  return prompt("");
 }
 
 const humanSelection = getHumanChoice().toLowerCase();
 
-
-
-
 // results
 
-// score 
+// score
 
-let humanScore = 0;
-let computerScore = 0;
+// let humanScore = 0;
+// let computerScore = 0;
+
+// function playRound(computerSelection, humanSelection) {
+
+//     console.log(computerSelection);
+
+//     console.log(humanSelection);
+
+//     if (computerSelection == "paper" && humanSelection == "rock") {
+
+//         console.log("You lose! Paper beats Rock");
+//         computerScore++;
+
+//     } else if (computerSelection == "rock" && humanSelection == "paper") {
+
+//        console.log("You won! Paper beats Rock");
+//        humanScore++;
+
+//     } else if (computerSelection == "rock" && humanSelection == "scissors") {
+
+//         console.log("You lose! Rock beats scissors");
+//         computerScore++;
+
+//     } else if (computerSelection == "paper" && humanSelection == "scissors") {
+
+//         console.log("You won! scissors beats Paper");
+//         humanScore++;
+
+//     }   else if (computerSelection == humanSelection) {
+
+//         console.log("Draw! it is a tie");
+
+//     }
+
+// }
+
+// playRound(computerSelection, humanSelection);
+
+getComputerChoice(str);
+getHumanChoice().toLowerCase();
 
 
-
-function playRound(computerSelection, humanSelection) {
-
-    console.log(computerSelection);
-
-    console.log(humanSelection);
+// 5 Rounds
 
 
-    if (computerSelection == "paper" && humanSelection == "rock") {
+function playGame() {
 
-        console.log("You lose! Paper beats Rock");
-        computerScore++;
+    let humanScore = 0;
+    let computerScore = 0;
 
-    } else if (computerSelection == "rock" && humanSelection == "paper") {
+    for (let i = 0; i<5; i++) {
 
-       console.log("You won! Paper beats Rock");
-       humanScore++;
+        function playRound(computerSelection, humanSelection) {
 
-    } else if (computerSelection == "rock" && humanSelection == "scissors") {
+            console.log(computerSelection);
+            console.log(humanSelection);
 
-        console.log("You lose! Rock beats scissors");
-        computerScore++;
+            if (computerSelection == "paper" && humanSelection == "rock") {
+                console.log("You lose! Paper beats Rock");
+                computerScore++;
 
-    } else if (computerSelection == "paper" && humanSelection == "scissors") {
+            } else if (computerSelection == "rock" && humanSelection == "paper") {
+                console.log("You won! Paper beats Rock");
+                humanScore++;
 
-        console.log("You won! scissors beats Paper");
-        humanScore++;
+            } else if (computerSelection == "rock" && humanSelection == "scissors") {
+                console.log("You lose! Rock beats scissors");
+                computerScore++;
 
-    }   else if (computerSelection == humanSelection) {
+            } else if (computerSelection == "paper" && humanSelection == "scissors") {
+                console.log("You won! scissors beats Paper");
+                humanScore++;
 
-        console.log("Draw! it is a tie");
+            } else if (computerSelection == humanSelection) {
+                console.log("Draw! it is a tie");
+
+            }
+
+        }
+
+        playRound(computerSelection, humanSelection);
 
     }
+   
+    if (computerScore > humanScore) {
+
+        console.log("You lost This Game!");
+
+    } else if (humanScore > computerScore) {
+
+        console.log("You Won This Game!");
+
+    } else {
+
+        console.log("It's a tie!");
+    }
+       
 
 }
 
-let play = playRound(computerSelection, humanSelection);
-
-
-
-// 5 Rounds 
-
-
-function playGame  (play)  {
-
-    for(let i=0; i<=5; i++) {
-
-       play;
-
-    }
-
-
-}
-
-playGame(play);
+playGame();
